dd280ff77a115c7115877724d8bf0514b9573804
parkjikuk <paxkk@naver.com>
2022-12-10 21:55:27 +0900

    Change Mapbox language with Korean

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -2,6 +2,7 @@ import React from 'react';
 import tw from "tailwind-styled-components"
 import mapboxgl from '!mapbox-gl';
 import { useEffect } from 'react';
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
 
 mapboxgl.accessToken = "pk.eyJ1IjoicGFya2pheSIsImEiOiJjbGI3MjVlaGIwNnk0M3RydnMzeGgyc2NoIn0.lRA8nUxA0ILyB6qkhVyPLA";
 
@@ -11,8 +12,11 @@ const Map = (props) => {
       container: "map",
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [127, 37],
-      zoom: 7,
+      zoom: 3,
     });
+
+    const language = new MapboxLanguage(['get', 'Korean']);
+    map.addControl(language);
 
     if(props.pickupCoordinates) {
       addToMap(map, props.pickupCoordinates)
@@ -24,7 +28,7 @@ const Map = (props) => {
 
     if(props.pickupCoordinates && props.dropoffCoordinates){
       map.fitBounds([props.pickupCoordinates, props.dropoffCoordinates], {
-        padding: 80
+        padding: 60
         })
     }
   }, [props.pickupCoordinates, props.dropoffCoordinates]);
@@ -43,5 +47,5 @@ const Map = (props) => {
 export default Map;
 
 const Wrapper = tw.div`
-flex-1
+flex-1 h-1/2
 `
