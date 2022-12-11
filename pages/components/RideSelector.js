ef0ac25b0f79ae254d2973f99b887584eccb333c
parkjikuk <paxkk@naver.com>
2022-12-11 23:59:57 +0900

    ✨feat : 차량 종류에 따라서 다른 가격 구하기

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import tw from "tailwind-styled-components"
 import { carList } from '../data/carList';
 
-const RideSelector = () => {
+const RideSelector = ( {pickupCoordinates, dropoffCoordinates}) => {
+  const [rideDuration, setRideDuration] = useState(0)
+
+  useEffect (() => {
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1IjoicGFya2pheSIsImEiOiJjbGI3MjVlaGIwNnk0M3RydnMzeGgyc2NoIn0.lRA8nUxA0ILyB6qkhVyPLA`)
+    .then(res => res.json())
+    .then(data => setRideDuration(data.routes[0].duration / 100))
+  },[pickupCoordinates, dropoffCoordinates])
+
   return (
       <Wrapper>
         <Title>Choose a ride, or swipe up for more</Title>
@@ -14,7 +22,7 @@ const RideSelector = () => {
               <Service>{car.service}</Service>
               <Time>5 min away</Time>
             </CarDetails>
-            <Price>$24.00</Price>
+            <Price>{'$' + (rideDuration * car.multiplier).toFixed(2)}</Price>
           </Car>
           ))}
           
