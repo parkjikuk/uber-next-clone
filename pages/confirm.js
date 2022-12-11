2f331bec9c74fccca7a4468e92c67e1434465160
parkjikuk <paxkk@naver.com>
2022-12-11 23:57:01 +0900

    Add pickupCoordinates and dropoffCoordinates inital value

--- pages/confirm.js
+++ pages/confirm.js
@@ -9,8 +9,8 @@ const Confirm = () => {
   const router = useRouter()
   const {pickup, dropoff} = router.query
 
-  const [ pickupCoordinates, setPickupCoordinates] = useState()
-  const [ dropoffCoordinates, setDropoffCoordinates] = useState()
+  const [ pickupCoordinates, setPickupCoordinates] = useState([0, 0])
+  const [ dropoffCoordinates, setDropoffCoordinates] = useState([0, 0])
 
   const getPickupCoordinates = (pickup) => {
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` + 
@@ -55,7 +55,10 @@ const Confirm = () => {
         dropoffCoordinates = {dropoffCoordinates}
       />
       <RideContainer>
-        <RideSelector />
+        <RideSelector
+          pickupCoordinates = {pickupCoordinates}
+          dropoffCoordinates = {dropoffCoordinates}
+        />
         <ConfirmButtonContainer>
           <ConfirmButton>
             Confirm UberX
